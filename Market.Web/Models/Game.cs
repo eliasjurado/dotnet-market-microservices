f64d800e7edbc16637d1ba012fda2248887d76bd
Elias Jurado <elias.jurado@globant.com>
2023-12-12 10:46:33 -0500

    add mock for get all coupons

---
+++ Market.Web/Models/Game.cs
@@ -0,0 +1,17 @@
+﻿namespace Market.Web.Models
+{
+    public record Game
+    {
+        public int Year { get; set; }
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public string Console { get; set; }
+        public Game(int year, string name, string publisher, string console)
+        {
+            Year = year;
+            Name = name;
+            Publisher = publisher;
+            Console = console;
+        }
+    }
+}
---
+++ Market.Web/Pages/Coupon.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using Htmx;
+using Market.Web.Models;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Market.Web.Pages
+{
+    public class Coupon : PageModel
+    {
+        private static List<Game> Games = new()
+        {
+            new Game(1993, "Super Mario Bros. 3", "Nintendo", "NES"),
+            new Game(1992, "The Legend of Zelda: A Link To The Past", "Nintendo", "SNES"),
+            new Game(1992, "Street Fighter II Turbo", "Capcom", "Arcade"),
+            new Game(1992, "Sonic The Hedgehog 2", "Sega", "Mega Drive"),
+            new Game(1986, "Outrun", "Sega", "Arcade"),
+            new Game(1978, "Space Invaders", "Taito", "Arcade"),
+            new Game(1992, "Streets Of Rage 2", "Sega", "Mega Drive"),
+            new Game(1994, "Super Metroid", "Nintendo", "SNES"),
+            new Game(1972, "Pong", "Atari", "Atari"),
+            new Game(1996, "Resident Evil", "Capcom", "Playstation"),
+            new Game(1995, "Chrono Trigger", "Squaresoft", "SNES")
+        };
+
+        [BindProperty(SupportsGet = true)]
+        public string? Query { get; set; }
+
+        public List<Game> Results { get; private set; }
+            = Games;
+
+        public IActionResult OnGet()
+        {
+            Results = string.IsNullOrEmpty(Query)
+                ? Games
+                : Games.Where(g => g.ToString().Contains(Query, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            if (!Request.IsHtmx())
+                return Page();
+
+            Response.Htmx(h =>
+            {
+                // we want to push the current url 
+                // into the history
+                h.Push(Request.GetEncodedUrl());
+            });
+
+            return Partial("_Results", this);
+        }
+    }
+}
