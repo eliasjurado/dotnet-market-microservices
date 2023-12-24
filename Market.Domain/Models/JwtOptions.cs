47f491098ca1d8f7bd418223834943ae1d11efa4
Elias Jurado <elias.jurado@globant.com>
2023-12-24 17:24:11 -0500

    refactor domain entities

--- Market.Domain/Models/JwtOptions.cs
+++ Market.Domain/Models/JwtOptions.cs
@@ -1,13 +1,9 @@
-﻿using Market.Domain.Models.Dto.Services.Coupon;
-
-namespace Market.Domain.Models.Dto.Services
+﻿namespace Market.Domain.Models
 {
     public class JwtOptions
     {
         public string Secret { get; set; } = string.Empty;
         public string Issuer { get; set; } = string.Empty;
         public string Audience { get; set; } = string.Empty;
-
-        new CouponRequestDto code { get; set; } = new CouponRequestDto { };
     }
 }
