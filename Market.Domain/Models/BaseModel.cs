2749b30a22dfd449432268bd48c58e3fbbe047ba
Elias Jurado <elias.jurado@globant.com>
2023-12-28 07:37:33 -0500

    add productapi project

---
+++ Market.Domain/Models/BaseModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Market.Infrastructure.Base;
+
+namespace Market.Domain.Models
+{
+    public class BaseModel
+    {
+        [Required]
+        public ByteType IsDeleted { get; set; } = ByteType.No;
+        [Required]
+        public string CreatedBy { get; set; } = DefaultUser;
+        [Required]
+        public string UpdatedBy { get; set; } = DefaultUser;
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        [Required]
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ Market.Domain/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Market.Domain.Models
+{
+    public class Product : BaseModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; } = 0;
+        [Required]
+        public string CategoryName { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+    }
+}
